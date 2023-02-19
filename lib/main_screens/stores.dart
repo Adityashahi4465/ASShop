e0f10af3f052e1d68040ec4f6f7574e1af881ace
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-02-19 11:22:59 +0530

    Streaming s

--- lib/main_screens/stores.dart
+++ lib/main_screens/stores.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../widgets/appbar_widgets.dart';
 
@@ -15,6 +17,61 @@ class StoresScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         title: const AppBarTitle(title: 'Stores'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('suppliers').snapshots(),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                itemCount: snapshot.data!.docs.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 25,
+                  crossAxisSpacing: 25,
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Stack(
+                        children: [
+                          SizedBox(
+                            height: 120,
+                            width: 120,
+                            child: Image.asset('images/inapp/store.jpg'),
+                          ),
+                          Positioned(
+                            bottom: 28,
+                            left: 10,
+                            child: SizedBox(
+                              height: 48,
+                              width: 100,
+                              child: Image.network(
+                                snapshot.data!.docs[index]['storelogo'],
+                                fit:  BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        snapshot.data!.docs[index]['storename'],
+                        style: GoogleFonts.akayaTelivigala(
+                          fontSize: 26,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+            return const Center(
+              child: Text('No Stores Available at this moment'),
+            );
+          },
+        ),
       ),
     );
   }
