79a3dcb7d852bd88c01dfe09ad02026fa974a466
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-02-19 11:07:37 +0530

    Adding zooming of images and list of images

--- lib/minor_screens/full_screen_view.dart
+++ lib/minor_screens/full_screen_view.dart
@@ -10,6 +10,8 @@ class FullScreenView extends StatefulWidget {
 }
 
 class _FullScreenViewState extends State<FullScreenView> {
+  final PageController _controller = PageController();
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -20,61 +22,83 @@ class _FullScreenViewState extends State<FullScreenView> {
         backgroundColor: Colors.white,
         leading: const AppBarBackButton(),
       ),
-      body: Column(
-        children: [
-          const Center(
-            child: Text(
-              '1/5',
-              style: TextStyle(
-                fontSize: 24,
-                letterSpacing: 1.8,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(
+              child: Text(
+                ('${index + 1} / ${widget.imagesList.length}'),
+                style: const TextStyle(
+                  fontSize: 24,
+                  letterSpacing: 1.8,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: height * 0.5,
-            child: PageView(
-              children: List.generate(
-                widget.imagesList.length,
-                (index) {
-                  return InteractiveViewer(
-                    transformationController: TransformationController(),
-                    child: Image.network(
-                      widget.imagesList[index].toString(),
-                    ),
-                  );
-                },
+            SizedBox(
+              height: height * 0.5,
+              child: PageView(
+                onPageChanged: (value) => setState(
+                  () {
+                    index = value;
+                  },
+                ),
+                controller: _controller,
+                children: images(),
               ),
             ),
-          ),
-          SizedBox(
-            height: height * 0.2,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.imagesList.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.all(8),
-                  width: 120,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 4,
-                        color: Colors.yellow,
-                      ),
-                      borderRadius: BorderRadius.circular(15)),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      widget.imagesList[index],
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
-              },
+            SizedBox(
+              height: height * 0.2,
+              child: imageView(),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget imageView() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: widget.imagesList.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            _controller.jumpToPage(index);
+          },
+          child: Container(
+            margin: const EdgeInsets.all(8),
+            width: 120,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 4,
+                  color: Colors.yellow,
+                ),
+                borderRadius: BorderRadius.circular(15)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                widget.imagesList[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  List<Widget> images() {
+    return List.generate(
+      widget.imagesList.length,
+      (index) {
+        return InteractiveViewer(
+          transformationController: TransformationController(),
+          child: Image.network(
+            widget.imagesList[index].toString(),
+          ),
+        );
+      },
     );
   }
 }
